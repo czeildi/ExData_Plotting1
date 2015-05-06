57dad2a8066dbb582b9f656072640df44d651d78
Czeller Ildikó <czeildi@users.noreply.github.com>
2015-05-06 17:12:52 +0200

    size adjusted

--- plot2.R
+++ plot2.R
@@ -6,7 +6,7 @@ data <- mutate(data, Date = as.Date(Date, "%d/%m/%Y"))
 data <- subset(data, subset = Date %in% c(as.Date("2007-02-01"),as.Date("2007-02-02")))
 
 data <- mutate(data, DateTime = as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S"))
-png(file = "plot2.png")
+png(file = "plot2.png", width = 480, height = 480)
 par(pty = "s")
 plot(data$DateTime,data$Global_active_power, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
 lines(data$DateTime, data$Global_active_power, type = "l")
