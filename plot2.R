2d77e9d6e1fe5078ef5d1eaa9a24757d04fe53a2
Czeller Ildikó <czeildi@users.noreply.github.com>
2015-05-06 17:36:27 +0200

    comments added

--- plot2.R
+++ plot2.R
@@ -1,13 +1,18 @@
 library(plyr)
-library(dplyr)
+library(dplyr) #this is necessary for the use of mutate
 
 data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?", as.is = TRUE)
 data <- mutate(data, Date = as.Date(Date, "%d/%m/%Y"))
 data <- subset(data, subset = Date %in% c(as.Date("2007-02-01"),as.Date("2007-02-02")))
 
+#combining date and time columns into one column
 data <- mutate(data, DateTime = as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S"))
+
+#opening a connection for plotting to png file
 png(file = "plot2.png", width = 480, height = 480)
-par(pty = "s")
+
 plot(data$DateTime,data$Global_active_power, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)")
 lines(data$DateTime, data$Global_active_power, type = "l")
+
+#closing device
 dev.off()
